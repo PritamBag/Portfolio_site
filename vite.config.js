7abd0d1c87b4e38d8989b98761ba0d7f77e33048
Pritam Bag <pritam02b@gmail.com>
2025-08-31 12:48:44 +0530

    Build site for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Portfolio_site/",
-  build:{
-    outDir:"./docs",
+  base: "/",  // Use "/" for custom domain
+  build: {
+    outDir: "./docs", // Keep docs if you want to deploy from docs folder
   },
 })
